8735020cb484b6a9e50888f79a51618871d0e1f9
MarianaBPOliveira <maribpoliveira@gmail.com>
2025-10-17 17:28:12 -0300

    Correção do erro e adição do download de totais

--- app.py
+++ app.py
@@ -162,12 +162,12 @@ else:
 # ==============================
 # BOTÃO PARA LIMPAR PEDIDOS
 # ==============================
-if os.path.exists(ARQUIVO_RELATORIO):
-    st.markdown("---")
-    if st.button("🗑️ Limpar todos os pedidos"):
-        os.remove(ARQUIVO_RELATORIO)
-        st.warning("Todos os pedidos foram apagados com sucesso!")
-        st.stop()
+#if os.path.exists(ARQUIVO_RELATORIO):
+#    st.markdown("---")
+#    if st.button("🗑️ Limpar todos os pedidos"):
+#        os.remove(ARQUIVO_RELATORIO)
+#        st.warning("Todos os pedidos foram apagados com sucesso!")
+#        st.stop()
 
 
 if os.path.exists(ARQUIVO_RELATORIO):
